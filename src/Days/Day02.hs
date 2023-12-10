6775659a7193c7830fe9de01091354b24fcbbf21
Jon Vuri <jrajav@gmail.com>
2023-12-10 10:15:51 -0600

    Complete day 2, part 1 and 2

--- src/Days/Day02.hs
+++ src/Days/Day02.hs
@@ -21,19 +21,103 @@ runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
 inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
+inputParser = many1 parseGame <* endOfInput
+
+parseGame :: Parser Game
+parseGame = do
+  _ <- string "Game "
+  gameId <- decimal
+  _ <- string ": "
+  gameRounds <- sepBy1 parseGameRound (string "; ") <* endOfLine
+  return $ Game {..}
+
+parseGameRound :: Parser GameRound
+parseGameRound = do
+  sepBy1 (choice [parseRedShown, parseGreenShown, parseBlueShown]) (string ", ")
+
+parseRedShown :: Parser ColorShown
+parseRedShown = do
+  colorNumber <- decimal
+  _ <- string " red"
+  return ColorShown {color = Red, ..}
+
+parseGreenShown :: Parser ColorShown
+parseGreenShown = do
+  colorNumber <- decimal
+  _ <- string " green"
+  return ColorShown {color = Green, ..}
+
+parseBlueShown :: Parser ColorShown
+parseBlueShown = do
+  colorNumber <- decimal
+  _ <- string " blue"
+  return ColorShown {color = Blue, ..}
 
 ------------ TYPES ------------
-type Input = Void
+type Input = [Game]
 
-type OutputA = Void
+type OutputA = Int
 
-type OutputB = Void
+type OutputB = Int
+
+data Color = Red | Green | Blue
+  deriving (Show, Eq, Ord)
+
+data ColorShown = ColorShown
+  { color :: Color,
+    colorNumber :: Int
+  }
+  deriving (Show)
+
+type GameRound = [ColorShown]
+
+data Game = Game
+  { gameRounds :: [GameRound],
+    gameId :: Int
+  }
+  deriving (Show)
 
 ------------ PART A ------------
+
+-- Filter out red colors above 12, green colors above 13, blue colors above 14
+isValidColorShown :: ColorShown -> Bool
+isValidColorShown ColorShown {color = Red, colorNumber} = colorNumber <= 12
+isValidColorShown ColorShown {color = Green, colorNumber} = colorNumber <= 13
+isValidColorShown ColorShown {color = Blue, colorNumber} = colorNumber <= 14
+
+isValidGameRound :: GameRound -> Bool
+isValidGameRound = all isValidColorShown
+
+isValidGame :: Game -> Bool
+isValidGame game = all isValidGameRound $ gameRounds game
+
+filterValidGames :: [Game] -> [Game]
+filterValidGames = filter isValidGame
+
 partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+partA input = sum $ map gameId (filterValidGames input)
 
 ------------ PART B ------------
+
+toNumbers :: GameRound -> [Int]
+toNumbers = map colorNumber
+
+filterColor :: Color -> GameRound -> GameRound
+filterColor color = filter (\ColorShown {color = c} -> c == color)
+
+-- Take max of a single color for a game across all rounds
+maxColorShown :: Color -> Game -> Int
+maxColorShown color game =
+  maximum . maximum $
+    map toNumbers (filter (not . null) (map (filterColor color) (gameRounds game)))
+
+gamePower :: Game -> Int
+gamePower game = do
+  product
+    [ maxColorShown Red game,
+      maxColorShown Green game,
+      maxColorShown Blue game
+    ]
+
 partB :: Input -> OutputB
-partB = error "Not implemented yet!"
+partB input = sum $ map gamePower input
